74508ab1f3378bdb603c1b645dcba5492c1ef5b5
Jinrong Pettit <jinpettit@gmail.com>
2023-11-16 20:25:26 -0800

    git Merge branch 'main' of https://github.com/jackherberger/Bud into jin

--- budapp/client/src/App.js
+++ budapp/client/src/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TransactionTable from "./TransactionTable";
-import CategoryPieChart from "./CategoryPieChart";
-import DateBarChart from "./DateBarChart";
-import "./App.css";
-import Login from "./login";
-import SignUp from "./signup";
-import Navbar from "./Navbar";
-import Home from "./Home";
+import React, { useState, useEffect } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import TransactionTable from "./TransactionTable"
+import CategoryPieChart from "./CategoryPieChart"
+import DateBarChart from "./DateBarChart"
+import "./App.css"
+import Login from "./login"
+import SignUp from "./signup"
+import Navbar from "./Navbar"
+import Home from "./Home"
 
 function App() {
   useEffect(() => {
     // Function to generate a random transaction
     const generateRandomTransaction = (id) => {
-      const randomName = `Item ${id}`;
-      const randomPrice = (Math.random() * 100).toFixed(2);
-      const year = 2023;
-      const month = Math.floor(Math.random() * 12) + 1;
-      const day = Math.floor(Math.random() * 28) + 1; // Choose any day within the month
-      const randomDate = `${year}-${month
+      const randomName = `Item ${id}`
+      const randomPrice = (Math.random() * 100).toFixed(2)
+      const year = 2023
+      const month = Math.floor(Math.random() * 12) + 1
+      const day = Math.floor(Math.random() * 28) + 1 // Choose any day within the month
+      const randomDate = `${year}-${month.toString().padStart(2, "0")}-${day
         .toString()
-        .padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
+        .padStart(2, "0")}`
       const categories = [
         "Groceries",
         "Clothes",
@@ -31,9 +31,9 @@ function App() {
         "Electronics",
         "Travel",
         "Other",
-      ];
+      ]
       const randomCategory =
-        categories[Math.floor(Math.random() * categories.length)];
+        categories[Math.floor(Math.random() * categories.length)]
 
       return {
         id,
@@ -41,19 +41,19 @@ function App() {
         price: parseFloat(randomPrice),
         date: randomDate,
         category: randomCategory,
-      };
-    };
-
-    // Generate 10 random transactions
-    const randomTransactions = [];
-    for (let id = 1; id <= 10; id++) {
-      randomTransactions.push(generateRandomTransaction(id));
+      }
     }
 
-    setTransactions(randomTransactions);
-  }, []); // Empty dependency array to run this effect only once
+    // Generate 10 random transactions
+    const randomTransactions = []
+    for (let id = 1; id <= 10; id++) {
+      randomTransactions.push(generateRandomTransaction(id))
+    }
 
-  const [transactions, setTransactions] = useState([]);
+    setTransactions(randomTransactions)
+  }, []) // Empty dependency array to run this effect only once
+
+  const [transactions, setTransactions] = useState([])
 
   // const handleAddTransaction = (newTransaction) => {
   //   // Update the transactions array by adding the new transaction
@@ -62,8 +62,8 @@ function App() {
 
   const onAddTransaction = (newTransaction) => {
     // Add the new transaction to your data state
-    setTransactions([...transactions, newTransaction]);
-  };
+    setTransactions([...transactions, newTransaction])
+  }
   return (
     <div className="App">
       <Router>
@@ -94,10 +94,11 @@ function App() {
               </div>
             }
           />
+          <Route path="/account" element={<AccountDisplay />} />
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
